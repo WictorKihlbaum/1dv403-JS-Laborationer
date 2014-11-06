6eed795e44c5b94603e640567b63207fc65381d1
WictorKihlbaum <wk222as@student.lnu.se>
2014-11-06 17:08:19 +0100

    JS Lab Vandalen
    Vandalen Almost Done, Error messages left

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -1,30 +1,43 @@
 "use strict";
 
-var makePerson = function(persArr) { 
-	
-	var result = {};
-	// Hämtar ut och sorterar namnen från 'persArr'.
-	var names = persArr.map(function (person){ return person.name; }).sort().join(", ");
-	console.log(names);
-	// Hämtar ut åldrar från 'persArr'.
-	var ages = persArr.map(function (ages){ return ages.age;});
-	console.log(ages);
-	// Räknar ut maxåldern.
-	var maxAge = ages.reduce(function(a,b){ return a+b;});
-	console.log(maxAge);
+	var makePerson = function(persArr) { 
+		
+		var result = {};
 
+		// Hämtar ut och sorterar ( Svenska tecken medräknat ) namnen från 'persArr'.
+		var names = persArr.map(function (names){ return names.name; }).sort(function(a, b) { return a.localeCompare(b) }).join(", ");
 
-	
-	
+		// Hämtar ut åldrar från 'persArr'.
+		var ages = persArr.map(function (ages){ return ages.age;});
 
-// Lös uppgiften
+		// Räknar ut antalet personer.
+		var persons = persArr.length;
 
-return result; 
+		// Adderar alla åldrar... 
+		var averageAge = ages.reduce(function(a,b){ return a+b;});
 
-} 
+		// ...och räknar därefter ut  ( avrundat ) medelvärde.
+		averageAge /= persons;
+		averageAge = Math.round(averageAge);
 
-var data = [{name: "John Häggerud", age: 37}, {name: "Johan Leitet", age: 36}, {name: "Mats Loock", age: 46}];
+		// Hämtar ut minsta ålder.
+		var minAge = Math.min.apply(Math, ages);
 
-var result = makePerson(data);
+		// Hämtar ut högsta ålder.
+		var maxAge = Math.max.apply(Math, ages);
 
-console.log(result);
+		// Adderar variabelvärden.
+		result.minAge = minAge;
+		result.maxAge = maxAge;
+		result.averageAge = averageAge;
+		result.names = names;
+
+	return result; 
+
+	} 
+
+	var data = [{name: "John Häggerud", age: 37}, {name: "Johan Leitet", age: 36}, {name: "Mats Loock", age: 46}];
+
+	var result = makePerson(data);
+
+	console.log(result);
