90f064c8ea9f9fe2ea41596d539eff6a0787ba3d
WictorKihlbaum <wk222as@student.lnu.se>
2014-12-03 14:45:31 +0100

    Memory
    I have managed to make a table with JS and put it in my main in HTML
    index. Got help from:
    https://developer.mozilla.org/en-US/docs/Traversing_an_HTML_table_with_JavaScript_and_DOM_Interfaces.
    
    Next step: put the pictures in instead of text

--- 3-gameon/memory/script/memory.js
+++ 3-gameon/memory/script/memory.js
@@ -5,12 +5,40 @@ var Memory = {
 	init:function()	{
 
 		var rows = 4;
-		var cols = 4;
+		var cols = 2;
 
 		var randomArray = RandomGenerator.getPictureArray(rows, cols);
 
-		console.log(random);
+		console.log(randomArray);
+		Memory.createTable(rows, cols);
+	},
 
+	createTable : function(rows, cols) {
+
+		var main = document.getElementsByTagName("main")[0];
+
+		var table = document.createElement("table");
+		var tableBody = document.createElement("tbody");
+
+		for (var i = 0; i < rows; i++) {
+
+			var row = document.createElement("tr");
+
+			for (var j = 0; j < cols; j++) {
+
+				var cell = document.createElement("td");
+				var cellText = document.createTextNode("cell in row "+i+", column "+j);
+
+				cell.appendChild(cellText);
+				row.appendChild(cell);
+			}
+
+			tableBody.appendChild(row);
+		}
+
+		table.appendChild(tableBody);
+		main.appendChild(table);
+		table.setAttribute("border", "2");
 
 	},
 
