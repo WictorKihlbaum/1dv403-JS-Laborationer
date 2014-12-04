b7f85bfc8109276dad20342937a1be1b452b2dcc
WictorKihlbaum <wk222as@student.lnu.se>
2014-12-04 14:14:19 +0100

    Memory
    Each a-tag has now a value and an image. You can click on each
    questionmark and they will show another image.

--- 3-gameon/memory/script/memory.js
+++ 3-gameon/memory/script/memory.js
@@ -7,30 +7,27 @@ var Memory = {
 		var rows = 4;
 		var cols = 4;
 		var randomArray = RandomGenerator.getPictureArray(rows, cols);
-		console.log(randomArray);
 
 		Memory.createTable(rows, cols, randomArray);
+		Memory.divideValues(randomArray);
 	},
 
 	createTable : function(rows, cols, randomArray) {
 
 		var memoryDiv = document.getElementById("memoryDiv");
-
 		var memoryTable = document.createElement("table");
 		var tableBody = document.createElement("tbody");
 
-		for (var r = 0; r < rows; r++) {
+		for (var r = 0; r < rows; r += 1) {
 
 			var row = document.createElement("tr");
 
-			for (var c = 0; c < cols; c++) {
+			for (var c = 0; c < cols; c += 1) {
 
 				var cell = document.createElement("td");
-
 				var cellImg = document.createElement("img");
 					cellImg.setAttribute("src", "pics/0.png");
 					cellImg.alt = "Show hidden image";
-
 				var cellImgLink = document.createElement("a");
 					cellImgLink.href = "#";
 					
@@ -44,22 +41,29 @@ var Memory = {
 
 		memoryTable.appendChild(tableBody);
 		memoryDiv.appendChild(memoryTable);
+	},
+
+	divideValues : function(randomArray) {
 
 		var aValues = document.getElementsByTagName("a");
 
-		for(var i = 0; i < randomArray.length; i += 1){
+		for (var i = 0; i < randomArray.length; i += 1) {
 			
 			aValues[i].rel = randomArray[i];
 		}
 
-		for(var i = 0; i < aValues.length; i += 1){
-			aValues[i].addEventListener("click", Memory.showImg, false);
+		for (var i = 0; i < aValues.length; i += 1) {
+			aValues[i].addEventListener("click", Memory.whenClick, false);
 		}
 	},
 
-	showImg : function(e) {
+	whenClick : function(e) {
+
 		e.preventDefault();
-		console.log(this.getAttribute("rel"));
+		console.log(this);
+		var aTag = this;
+		var rel = aTag.getAttribute("rel");
+		aTag.firstChild.setAttribute("src", "pics/" + rel + ".png");
 	},
 
 };
