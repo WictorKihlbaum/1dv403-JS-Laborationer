7166282d5e2a1e9ff02886a8f2f0c0834308d739
WictorKihlbaum <wk222as@student.lnu.se>
2015-01-05 17:29:01 +0100

    Quiz
    send function updated

--- 4-thequiz/script/Quiz.js
+++ 4-thequiz/script/Quiz.js
@@ -2,6 +2,8 @@
 
 var Quiz = {
 
+	nextURL: "http://vhost3.lnu.se:20080/question/1",
+
 	init: function() {
 		Quiz.getQuestion();
 		document.getElementById("sendButton").addEventListener("click", Quiz.sendAnswer, false);
@@ -18,41 +20,48 @@ var Quiz = {
 				var question = JSON.parse(xhr.responseText);
 				document.getElementById("questionField").innerHTML = question.question;
 				console.log(question);
+				Quiz.nextURL = question.nextURL;
 			}
 			else {
 
-				console.log("Läsfel!" + xhr.status);
+				console.log("Läsfel! " + xhr.status);
 			}
 		}; 
 
-		xhr.open("GET", "http://vhost3.lnu.se:20080/question/1", true);
+		xhr.open("GET", Quiz.nextURL, true);
 		xhr.send(null);
 	},
 
 	sendAnswer: function() {
 		
-		alert("hej");
 		var userAnswer = document.getElementById("answerField").value;
 		var xhr = new XMLHttpRequest();
 
 		xhr.onreadystatechange = function() {
 
-			if (xhr.readyState === 4 && xhr.status === 200) {
+			if (xhr.readyState === 4) {
 
-				var answer = JSON.parse(xhr.responseText);
+				if (xhr.status === 200) {
+
+					var answer = JSON.parse(xhr.responseText);
+				}	
+				else {
+					
+					var wrongAnswer = "Fel svar! Försök igen";
+					document.getElementById("questionField").innerHTML = wrongAnswer;
+				}
 			}
 			else {
-				console.log("Läsfel!" + xhr.status);
+				console.log("Läsfel! " + xhr.status);
 			}
 		};
 
-		xhr.open("POST", "http://vhost3.lnu.se:20080/answer/1", true);
+		xhr.open("POST", Quiz.nextURL, true);
 		xhr.setRequestHeader("content-Type", "application/json");
 
 		var answer = {
 
-			"id": 125,
-			"name": "Answer"
+			"answer": userAnswer
 		};
 
 		xhr.send(JSON.stringify(answer));
